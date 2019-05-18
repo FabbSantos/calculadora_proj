03a219c980f54b77404c2d1fcedd75abfca3b043
Fabricio Santos <fabriciobs2000@gmail.com>
2019-05-18 19:36:21 -0300

    Feitas melhorias no código. Definição de funções para cada operação e criação de variáveis para os valores passados pelo terminal e tratamento para erro.

--- calculadora.py
+++ calculadora.py
@@ -1,22 +1,38 @@
 # -*- coding: utf-8 -*- 
 import sys
 import math
+arg1 = sys.argv[1]
+arg2 = sys.argv[3]
+def soma(arg1, arg2):
+    print(arg1 + arg2)
 
-if sys.argv[2] == '+':
-    output = float(sys.argv[1]) + float(sys.argv[3])
-    print (output)
-elif sys.argv[2] == '-':
-    output = float(sys.argv[1]) - float(sys.argv[3])
-    print(output)
-elif sys.argv[2] == '*':
-    output = float(sys.argv[1]) * float(sys.argv[3])
-    print (round(output, 2))
-elif sys.argv[2] == '/':
-    output = float(sys.argv[1]) / float(sys.argv[3])
-    print (round(output, 2))
-elif sys.argv[2] == '**':
-    output = math.pow(float(sys.argv[1]), float(sys.argv[3]))
-    print (round(output, 2))
-elif sys.argv[2] == 'mod':
-    output = float(sys.argv[1]) % float(sys.argv[3])
-    print (round(output, 2))
+def subt(arg1, arg2):
+    print(arg1 - arg2)
+
+def mult(arg1, arg2):
+    print(round((arg1 * arg2), 2))
+
+def div(arg1, arg2):
+    print(round((arg1 / arg2), 2))
+
+def pot(arg1, arg2):
+    print(round(math.pow(arg1, arg2), 2))
+
+def mod(arg1, arg2):
+    print(round((arg1 % arg2), 2))
+
+def main():
+    if sys.argv[2] == '+':
+       soma(arg1, arg2)
+    elif sys.argv[2] == '-':
+        subt(arg1, arg2)
+    elif sys.argv[2] == '*':
+        mult(arg1, arg2)
+    elif sys.argv[2] == '/':
+        div(arg1, arg2)
+    elif sys.argv[2] == '**':
+        pot(arg1, arg2)
+    elif sys.argv[2] == 'mod':
+        mod(arg1, arg2)
+    else: print("Erro: Operação inválida.")
+main()
