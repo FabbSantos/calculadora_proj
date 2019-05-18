01476b3fa0281139dd45388720ed1d5500ab43a7
Fabricio Santos <fabriciobs2000@gmail.com>
2019-05-18 18:40:20 -0300

    Implementando divisão

--- calculadora.py
+++ calculadora.py
@@ -11,3 +11,6 @@ elif sys.argv[2] == '-':
 elif sys.argv[2] == '*':
     output = float(sys.argv[1]) * float(sys.argv[3])
     print (round(output, 2))
+elif sys.argv[2] == '/':
+    output = float(sys.argv[1]) / float(sys.argv[3])
+    print (round(output, 2))
