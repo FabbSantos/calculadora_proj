68ab161fbc1d820b4ec37117f66548cd59a93bd2
Fabricio Santos <fabriciobs2000@gmail.com>
2019-05-18 18:27:03 -0300

    Implementando subtração

--- calculadora.py
+++ calculadora.py
@@ -2,5 +2,8 @@
 import sys
 
 if sys.argv[2] == '+':
-    soma = int(sys.argv[1]) + int(sys.argv[3])
-    print (soma)
+    output = float(sys.argv[1]) + float(sys.argv[3])
+    print (output)
+elif sys.argv[2] == '-':
+    output = float(sys.argv[1]) - float(sys.argv[3])
+    print(output)
