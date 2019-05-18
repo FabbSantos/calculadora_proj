fb1dc6488e2429e1e0f2629a01b64beb4fc6d64d
Fabricio Santos <fabriciobs2000@gmail.com>
2019-05-18 18:38:39 -0300

    Implementando multiplicação

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*- 
 import sys
+import math
 
 if sys.argv[2] == '+':
     output = float(sys.argv[1]) + float(sys.argv[3])
@@ -7,3 +8,6 @@ if sys.argv[2] == '+':
 elif sys.argv[2] == '-':
     output = float(sys.argv[1]) - float(sys.argv[3])
     print(output)
+elif sys.argv[2] == '*':
+    output = float(sys.argv[1]) * float(sys.argv[3])
+    print (round(output, 2))
