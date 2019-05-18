141b5c06d45a0522f0b2a2d153265b218d48155a
Fabricio Santos <fabriciobs2000@gmail.com>
2019-05-18 19:50:55 -0300

    Adicionado comentarios e consertado um erro na criacao do arg1 e arg 2. Adicionado também uma variável contendo o argumento operador.

--- calculadora.py
+++ calculadora.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*- 
 import sys
 import math
-arg1 = sys.argv[1]
-arg2 = sys.argv[3]
+
+# Criação de variáveis para armazenar os valores passados pelo terminal
+arg1 = float (sys.argv[1])
+arg2 = float (sys.argv[3])
+argOperador = sys.argv[2]
+
+# Definição das funções 
 def soma(arg1, arg2):
     print(arg1 + arg2)
 
@@ -22,17 +27,19 @@ def mod(arg1, arg2):
     print(round((arg1 % arg2), 2))
 
 def main():
-    if sys.argv[2] == '+':
+    if argOperador == '+':
        soma(arg1, arg2)
-    elif sys.argv[2] == '-':
+    elif argOperador == '-':
         subt(arg1, arg2)
-    elif sys.argv[2] == '*':
+    elif argOperador == '*':
         mult(arg1, arg2)
-    elif sys.argv[2] == '/':
+    elif argOperador == '/':
         div(arg1, arg2)
-    elif sys.argv[2] == '**':
+    elif argOperador == '**':
         pot(arg1, arg2)
-    elif sys.argv[2] == 'mod':
+    elif argOperador == 'mod':
         mod(arg1, arg2)
-    else: print("Erro: Operação inválida.")
+    else: 
+        print("Erro: Operação inválida.")
+
 main()
